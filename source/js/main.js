3b914c112818a7ed91e60d6962b7f990d5a3320e
Svetlana Leovochkina <sleovochkina@gmail.com>
2020-05-19 21:33:17 +0300

    Добавляет адаптивную графику

--- source/js/main.js
+++ source/js/main.js
@@ -9,17 +9,25 @@ mobileMenuButton.addEventListener('click', function() {
   this.classList.toggle('page-header__toggle--off');
 });
 
-var orderButtons = document.querySelectorAll(".weekly-product__order-button, .catalog__cart-button, .video__order-button");
-var orderPopup = document.querySelector(".modal-size-selection");
-
+var orderButtons = document.querySelectorAll('.weekly-product__order-button, .catalog__cart-button, .video__order-button');
+var addButton = document.querySelector('.modal-size-selection__button');
+var orderPopup = document.querySelector('.modal-size-selection');
 
 if (orderPopup) {
   for (var i = 0; i < orderButtons.length; i++) {
-      orderButtons[i].addEventListener('click', function(evt) {
-        evt.preventDefault();
-        orderPopup.classList.add('modal--open');
-      });
+    orderButtons[i].addEventListener('click', function(evt) {
+    evt.preventDefault();
+    orderPopup.classList.add('modal--open');
+    });
   }
+
+  addButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    if (orderPopup.classList.contains('modal--open')) {
+      evt.preventDefault();
+      orderPopup.classList.remove('modal--open');
+    }
+  });
 
   window.addEventListener('keydown', function(evt) {
     if (evt.keyCode === 27) {
